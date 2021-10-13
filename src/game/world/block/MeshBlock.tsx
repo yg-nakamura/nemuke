db48d8cf740daba62e9fe318c25c31166a5b8c6f
yugorbit <yugo.nakamura.0101@gmail.com>
2021-10-14 02:18:42 +0900

    wip

--- src/game/world/block/MeshBlock.tsx
+++ src/game/world/block/MeshBlock.tsx
@@ -1,63 +1,64 @@
 import * as THREE from 'three';
 
-export const enum Block { 
+export const enum Block {
     Air,
     Stone
 }
 
-const enum Side{
-    PX ,
-    NX ,
-    PY ,
-    NY ,
-    PZ ,
+const enum Side {
+    PX,
+    NX,
+    PY,
+    NY,
+    PZ,
     NZ
 }
 
 
-const DEFAULT_PATH = "sides/" 
-const Blocks : { [key : number] : MeshBlock } = {};
-const Sides : { [key: number] : BlockSide[] }  = {};
+const DEFAULT_PATH = "texture/"
+const Blocks: { [key: number]: MeshBlock } = {};
+const Sides: { [key: number]: { [key: number]: BlockSide } } = {};
 const SIMPLE_BOX_GEOMETRY = new THREE.BoxGeometry(1, 1, 1);
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 
-class MeshBlock{
-    
-    sides : BlockSide[];
+class MeshBlock {
 
-    constructor(sides : BlockSide[]){
+    sides: { [key: number]: BlockSide };
+
+    constructor(sides: { [key: number]: BlockSide }) {
         this.sides = sides;
     }
 
-    public getMesh(x : number, y :number, z :number) : THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial[]>{
+    public getMesh(x: number, y: number, z: number): THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial[]> {
 
         const box = new THREE.Mesh(geometry, [
             this.sides[Side.PX].getSide(),
-            this.sides[Side.PX].getSide(),
-            this.sides[Side.PX].getSide(),
-            this.sides[Side.PX].getSide(),
-            this.sides[Side.PX].getSide(),
-            this.sides[Side.PX].getSide()
+            this.sides[Side.NX].getSide(),
+            this.sides[Side.PY].getSide(),
+            this.sides[Side.NY].getSide(),
+            this.sides[Side.PZ].getSide(),
+            this.sides[Side.NZ].getSide()
         ]);
         return box;
 
     }
 }
 
-class BlockSide{
+class BlockSide {
 
-    path :string = "";
-    texture : THREE.Texture;
-    
-    constructor(path : string){
+    path: string = "";
+    texture: THREE.Texture;
+
+    constructor(path: string) {
 
         this.path = path;
         const loader = new THREE.TextureLoader()
         this.texture = loader.load(DEFAULT_PATH + path);
+        this.texture.magFilter = THREE.NearestFilter;
 
     }
 
-    public getSide() : THREE.MeshBasicMaterial{
+    public getSide(): THREE.MeshBasicMaterial {
         return new THREE.MeshBasicMaterial({
             map: this.texture
         });
@@ -66,32 +67,37 @@ class BlockSide{
 
 
 
-function registersides(id : Block, texture : BlockSide) {
-    if(Sides[id]){
-        Sides[id].push(texture);
-    }else{
-        Sides[id] = [];
-        registersides(id, texture);
+function registerSide(id: Block, texture: BlockSide, side?: Side) {
+    if (side) {
+        if (Sides[id]) {
+            Sides[id][side] = texture;
+        }
+    } else {
+        registerSide(id, texture, Side.PX);
+        registerSide(id, texture, Side.NX);
+        registerSide(id, texture, Side.PY);
+        registerSide(id, texture, Side.NY);
+        registerSide(id, texture, Side.PZ);
+        registerSide(id, texture, Side.NZ);
+
     }
+
 }
 
-function registerBlock(id : Block) {
+function registerBlock(id: Block) {
     Blocks[id] = new MeshBlock(Sides[id]);
 }
 
-export function getBlock(block : Block) : THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial[]>{
-    return Blocks[block].getMesh(0,0,0);
+export function getBlock(block: Block): THREE.Mesh<THREE.BoxGeometry, THREE.MeshBasicMaterial[]> {
+    return Blocks[block].getMesh(0, 0, 0);
 }
 
 export function registerBlocks() {
 
     const stone_texture = new BlockSide("stone.png");
-    registersides(Block.Stone, stone_texture);
-    registersides(Block.Stone, stone_texture);
-    registersides(Block.Stone, stone_texture);
-    registersides(Block.Stone, stone_texture);
-    registersides(Block.Stone, stone_texture);
-    registersides(Block.Stone, stone_texture);
+    const grass_texture = new BlockSide("grass_carried.png");
+    registerSide(Block.Stone, stone_texture);
+
     registerBlock(Block.Stone);
 
 }
